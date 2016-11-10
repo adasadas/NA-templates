10dd9d16175ac2179378aa2c74f887e95811c198
Adam Suchodolski <adam.suchodolski@yahoo.com>
2016-11-10 15:09:01 +0000

    Adam Suchodolski 40-algos

--- 40-algos-impl/find_if.h
+++ 40-algos-impl/find_if.h
@@ -1,8 +1,20 @@
 #pragma once
 
-namespace training
+namespace training 
 {
-template<typename InputIt, typename UnaryPredicate>
-InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+	template<typename InputIt, typename UnaryPredicate> 
+	InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred) 
+	{
+
+		while (begin!=end) 
+		{
+			if (pred(*begin)) 
+			return begin;
+			++begin;
+		}
+
+	return end;
+	}
+
 }
 
--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,13 @@
 #pragma once
 
 // define vector_erase here
+#include <vector>
+
+template<typename T>
+void vector_erase(T & vec, const typename T::value_type & value)
+{
+	vec.erase(std::remove(vec.begin(), vec.end(), value), vec.end());
+
+}
+
 
