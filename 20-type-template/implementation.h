7a41c8a4df73f49d62ab7a5ee1b9d70521db6548
Adam Suchodolski <adam.suchodolski@yahoo.com>
2016-11-10 10:53:13 +0000

    new implementation

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,47 @@
 #pragma once
+#include <cstddef>
+#include <utility>
+#include <boost/optional.hpp>
 
 template<typename T>
-class container_wrapper;
+class container_wrapper{
+public:
+	container_wrapper() = default;
 
+	container_wrapper(T arg) : _value(std::move(arg)){}
+	
+	std::size_t size() const{
+		return _value.size();
+	}
+	
+	container_wrapper(const container_wrapper &) = default;
+	container_wrapper(container_wrapper &&) = default;
+	container_wrapper & operator=(const container_wrapper &) = default;
+	container_wrapper & operator=(container_wrapper &&) = default;
+	
+
+private:
+T _value;
+};
+
+
+template<typename T>
+class container_wrapper<boost::optional<T>>{
+public:
+	container_wrapper() = default;
+
+	container_wrapper(boost::optional<T> arg) : _value(std::move(arg)){}
+	
+	std::size_t size() const{
+		return _value ? 1 : 0;
+	}
+	
+	container_wrapper(const container_wrapper &) = default;
+	container_wrapper(container_wrapper &&) = default;
+	container_wrapper & operator=(const container_wrapper &) = default;
+	container_wrapper & operator=(container_wrapper &&) = default;
+	
+
+private:
+	boost::optional<T> _value;
+};
